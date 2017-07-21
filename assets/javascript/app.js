6299307aa7b7943639bacfebeb407d634bd98e2b
Kevin Martzolf <kmart914@gmail.com>
2017-07-21 09:54:20 -0700

    switched the funcitons outside of each question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
+$(document).ready(function() {
+
 var one = {
     question:"John",
-    answerChoices:["Doe","100","blue","red"]
+    answerChoices:["Doe","100","blue","red"],
+    answer:[true, false, false, false]
 };
 
 var two = {
@@ -69,32 +72,48 @@ var twelve = {
 
 };
 
+var questions = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve];
 
-$("#gameStart").click(function(){
+function questionSelect(showQuestion){
+  $("#title").html('<button class="btn">' + questions[showQuestion].question + '</button>')
+  $("#answer1").html('<button class="btn">' + questions[showQuestion].answerChoices[0] + '</button>');
+  $("#answer2").html('<button class="btn">' + questions[showQuestion].answerChoices[1] + '</button>');
+  $("#answer3").html('<button class="btn">' + questions[showQuestion].answerChoices[2] + '</button>');
+  $("#answer4").html('<button class="btn">' + questions[showQuestion].answerChoices[3] + '</button>');
+
+  console.log(showQuestion);
+}
 
 
 
-     $("#title").replaceWith('<h1>' + one.question + ' </h1>');
+//$("#gameStart").click(function(){
 
-      $("#answer1").html('<button class="btn">' + one.answerChoices[0] + '</button>');
 
-      $("#answer2").html('<button class="btn">' + one.answerChoices[1] + '</button>');
+//   $("#title").html('<h1>' + one.question + ' </h1>')
+//      $("#answer1").html('<button class="btn">' + one.answerChoices[0] + '</button>');
 
-      $("#answer3").html('<button class="btn">' + one.answerChoices[2] + '</button>');
+//      $("#answer2").html('<button class="btn">' + one.answerChoices[1] + '</button>');
 
-      $("#answer4").html('<button class="btn">' + one.answerChoices[3] + '</button>');
+//      $("#answer3").html('<button class="btn">' + one.answerChoices[2] + '</button>');
 
-      $(".btn-lg").hide();
+//      $("#answer4").html('<button class="btn">' + one.answerChoices[3] + '</button>');
 
-      var userGuess = 1
-      $("#answer1").click(function(){
-        userGuess = 1
-  });
+//      $(".btn-lg").hide();
 
-      if (userGuess === 1){
-        alert("You got it!");
-      } else {
-        alert("Wrong!");
-      }
-
-});
+//     var userGuess = 1
+//     $("#answer1").click(function(){
+//     userGuess = 1;
+//    });
+//   var userGuess = 0
+//    $("#answer2 #answer3 #answer4").click(function(){
+//      userGuess = 0;
+//    });
+//
+//    if( userGuess === 1){
+//      alert("WIN");
+//    } else {
+//      alert("You Lose!");
+//    }
+//
+//
+ });
