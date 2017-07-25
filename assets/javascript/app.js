de1337225724944404a8ebbc86aee527424da68d
unknown <kmart914@gmail.com>
2017-07-24 19:29:49 -0700

    changed object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,78 @@
 $(document).ready(function() {
 
-var questions = [
+// Object conatin questions, answers, and boolean to compare the user choice to the correct answer.
+var data = [
+    {
+      id: 1,
+      question:"What belongs to you but others use it more than you do?",
+      answerChoices:["Your Pencil","Your Money","Your Name","Your Car"],
+      answer: "Your Name",
 
+  },
   {
-    question:"John",
-    answerChoices:["Doe","100","blue","red"],
-    answer:[true, false, false, false]
-},
-{
-    question:"John",
-    answerChoices:["Doe","100","blue","red"]
-},
-{
-    question:"John",
-    answerChoices:["Doe","100","blue","red"]
+      id: 2,
+      question:"What has a Heart but no other organs?",
+      answerChoices:["A Duck","A Lizard","A Deck of Cards","AI"],
+      answer: "A Deck of Cards",
+  },
+  {
+    id: 3,
+      question:"The more you take, the more you leave behind. What am I?",
+      answerChoices:["Food","Footsteps","Money","Water"],
+      answer: "Footsteps",
 
-},
-{
-    question:"John",
-    answerChoices:["Doe","100","blue","red"]
+  },
+  {
+    id: 4,
+      question:" Imagine you're in a dark room that is perfectly empty with nothing in it. There are no windows or doors. What is the easiest way to escape?",
+      answerChoices:["Stop Imagining You Are In That Room","Die","Wait","Dig A Hole With Your Bare Hands"],
+      answer: "Stop Imagining You Are In That Room"
+  },
+  {
+    id: 5,
+      question:"If you look you cannot see me. And if you see me you cannot see anything else. I can make anything you want happen, but later everything goes back to normal. What am I?",
+      answerChoices:["Air","Nothing","Imagination","Clouds"],
+      answer: "Imagination"
 
-},
-{
-    question:"John",
-    answerChoices:["Doe","100","blue","red"]
+  },
+  {
+    id: 6,
 
-},
-{
-    question:"John",
-    answerChoices:["Doe","100","blue","red"]
+      question:"(This is fairly easy, but it'll have an impact on your final result!) If there are 100000000 bricks on a plane, and one falls off, how many are left?",
+      answerChoices:["99999999","100000000","11","9999999"],
+      answer: "1"
 
-},
-{
-    question:"John",
-    answerChoices:["Doe","100","blue","red"]
+  },
+  {
+    id: 7,
 
-},
-{
-    question:"John",
-    answerChoices:["Doe","100","blue","red"]
+      question:"What walks on four legs in the morning, two in the afternoon and three in the evening?",
+      answerChoices:["A robot with special programming", "A monkey who gets his leg cut off, grows them back, then loses them again ","A Human","Someone who is demented"],
+      answer: "A Human"
 
-},
-{
-    question:"John",
-    answerChoices:["Doe","100","blue","red"]
+  },
+  {
+    id: 8,
+      question:"A woman is walking, then suddenly dies in the middle of the street. How",
+      answerChoices:[" She was run over by a car","She was hit with a brick","She was shot","She was poisoned"],
+      answer: "A Brick"
+  },
+  {
+    id: 9,
 
-},
-{
-    question:"John",
-    answerChoices:["Doe","100","blue","red"]
+      question:"A man is going to a party. Along the way he meets a man with seven wives. Each of the wives as seven bags. Each of the bags have seven cats. Each of the cats has seven kittens. Each of the kittens has seven toys. How many people are going to the party?",
+      answerChoices:["16,807","9","1","0"],
+      answer: "1"
 
-},
-{
-    question:"John",
-    answerChoices:["Doe","100","blue","red"]
 
-},
-{
-    question:"John",
-    answerChoices:["Doe","100","blue","red"]
+  },
+  {
+    id: 10,
+    question:"Adriana's mom had four kids: Marta, Anna, Justina...",
+    answerChoices:["Annabell","Chloe","Adriana","Cleopatra"],
+    answer: "Adrianna"
 
-}
+  }
 ]
 
 var correctGif = ['<iframe src="https://giphy.com/embed/vggLJGHF1dgTC" width="480" height="319" style="position:relative; left: 30%; frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',]
@@ -69,78 +81,79 @@ var wrongGif = ['<iframe src="https://giphy.com/embed/hPPx8yk3Bmqys" width="480"
 
 var totalCorrect = 0;
 var totalWrong = 0;
+var index = 0;
 
-
-
+// Function to pull the first question at game start.
 function questionSelect(showQuestion){
   $("#title").html('<h2>' + questions[showQuestion].question + '</h2>')
-  $("#answer1").html('<button class="btn">' + questions[showQuestion].answerChoices[0] + '</button>');
-  $("#answer2").html('<button class="btn">' + questions[showQuestion].answerChoices[1] + '</button>');
-  $("#answer3").html('<button class="btn">' + questions[showQuestion].answerChoices[2] + '</button>');
-  $("#answer4").html('<button class="btn">' + questions[showQuestion].answerChoices[3] + '</button>');
+  $("#gameZone").append('<button class="btn answers" id="answer1">' + questions[showQuestion].answerChoices[0] + '</button>');
+  $("#gameZone").append('<button class="btn answers" id="answer2">' + questions[showQuestion].answerChoices[1] + '</button>');
+  $("#gameZone").append('<button class="btn answers" id="answer3">' + questions[showQuestion].answerChoices[2] + '</button>');
+  $("#gameZone").append('<button class="btn answers" id="answer4">' + questions[showQuestion].answerChoices[3] + '</button>');
 
   console.log(showQuestion);
 }
 
 
-
+//Game start function
 function start(){
 
   $('#gameStart').on('click', function() {
-      index = 0
-  		$(this).hide();
+    		$(this).hide();
       questionSelect(index);
-  		//countdownTimer.start();
   	});
 }
 
+//Function to populate next question after completing each question.
 function nextQuestion(){
   $('#nextQuestion').on('click', function() {
-    questionSelect(index++);
+    questionSelect(++index);
     hideThisStuff();
+
 
   });
 }
 
-function hideThisStuff(){
-  $('#nextQuestion').hide();
-  $('#gif').hide();
-  $(".correct").hide();
-  $(".wrong").hide();
+//Hide all of the stuff from the page after they select an answer so we can inject the next question in.
+//function hideThisStuff(){
+//  $('#nextQuestion').hide();
+//  $('#gif').hide();
+//  $(".correct").hide();
+//  $(".wrong").hide();
 
 
 
-}
+//}
 
+//If they get the answer correct this will run.
 function correct(){
   totalCorrect++;
   $("#title").replaceWith('<h2>' + "You Got It!" + '</h2>')
-  $('#gif').append(correctGif[0]);
-  $(".answers").hide();
-  $("#nextQuestion").html('<button class="btn">' + "Next Question" + '</button>');
+  $('#gameZone').replaceWith(correctGif[0]);
+//$("#nextQuestion").html('<button class="btn">' + "Next Question" + '</button>');
 
 
 }
-
+//If they get the answer wrong this will run.
 function wrong(){
   totalWrong++;
   $("#title").replaceWith('<h2>' + "Nope!" + '</h2>')
-  $('#gif').append(wrongGif[0]);
-  $(".answers").hide();
-  $("#nextQuestion").html('<button class="btn">' + "Next Question" + '</button>');
+  $('#gameZone').replaceWith(wrongGif[0]);
+//  $("#nextQuestion").html('<button class="btn">' + "Next Question" + '</button>');
 
 
 }
-
+//Function to show their current score after they select an answer.
 function score(){
-  $(".correct").html('<h2>' + "Total Correct: " + totalCorrect + '</h2>')
-  $(".wrong").html('<h2>' + "Total Wrong: " + totalWrong + '</h2>')
+  $("#quiz").append('<h2>' + "Total Correct: " + totalCorrect + '</h2>')
+  $("#quiz").append('<h2>' + "Total Wrong: " + totalWrong + '</h2>')
 }
 
+//Start game.
 start();
 
-
-$('.answers').on('click', function() {
+//Setting the value of each answer to a letter that we can tie back to the booleans.
+$('#quiz').on('click', function() {
  console.log($(this));
  if(this.id === "answer1"){
    var guess = 'A';
@@ -153,7 +166,17 @@ $('.answers').on('click', function() {
 }
 console.log(guess)
 
+//This will look for the
 if((guess === 'A') && (questions[index].answer[0] === true)){
+  correct();
+  score();
+} else if((guess ==='B') && (questions[index].answer[1] === true)){
+  correct();
+  score();
+} else if((guess ==='C') && (questions[index].answer[2] === true)){
+  correct();
+  score();
+} else if((guess ==='D') && (questions[index].answer[3] === true)){
   correct();
   score();
 } else {
